33b443958de7b1409857d64079e63c348b29b99f
J2523-Sou-mainlaptop <j2523@matsue-ct.ac.jp>
2026-04-10 18:18:18 +0900

    サーボモータのサンプル追加

---
+++ servo_esp32/servo_esp32.ino
@@ -0,0 +1,33 @@
+/*
+
+  J2523 髙橋湊
+  サーボモーター（ESP32・SG90使用）サンプルプログラム
+  動作確認：未
+
+*/
+
+//サーボモータライブラリ
+#include <ESP32Servo.h>
+
+//サーボのクラスを作成する
+Servo servo;
+
+void setup() {
+
+  //0番ピンをservoに紐づける
+  servo.attach(0);
+
+}
+
+void loop() {
+
+  //角度を増加させる
+  for (int i = 0; i <= 180; i++) {
+    servo.Write(i);
+  }
+  //角度を減少させる
+  for (int i = 180; i >= 0; i--) {
+    servo.Write(i);
+  }
+
+}
